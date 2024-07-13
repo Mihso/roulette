bd86d1d51876a5744f7d9fc5a5b205a4cb8dc565
Matthew Oshimo <moshimo90696@gmail.com>
2024-07-13 01:50:12 -0700

    added user model and apis

---
+++ backend/backend_api/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from backend_api.views import (
+    api_show_account,
+    api_list_accounts,
+    api_user_token,
+    neo_authenticate,
+    neo_logout,
+    get_some_data,
+    check_user,
+    update_censors,
+)
+
+urlpatterns = [
+    path("login/", neo_authenticate, name="login"),
+    path("censor/<int:pk>", update_censors, name="censors"),
+    path("logout/", neo_logout, name="logout"),
+    path("accounts/", api_list_accounts, name="accounts_list"),
+    path("accounts/<int:pk>", api_show_account, name="account_detail"),
+    path("tokens/mine/", api_user_token, name="get_token"),
+    path("get/token/", get_some_data, name="token_get"),
+    path("check/", check_user, name="check"),
+]
---
+++ backend/backend_api/views.py
@@ -0,0 +1,168 @@
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.hashers import make_password
+from django.http import JsonResponse
+# import djwto.authentication as auth
+from .models import User
+from common.json import ModelEncoder
+from django.views.decorators.http import require_http_methods
+import json
+
+
+# Create your views here.
+class AccountListEncoder(ModelEncoder):
+    model = User
+    properties = ["first_name", "id", "username"]
+
+
+class AccountDetailEncoder(ModelEncoder):
+    model = User
+    properties = [
+        "email",
+        "first_name",
+        "last_name",
+        "username",
+        "password",
+        "is_active",
+        "date_joined",
+        "censored",
+    ]
+
+
+@require_http_methods(["GET"])
+def api_user_token(request):
+    if "jwt_access_token" in request.COOKIES:
+        token = request.COOKIES["jwt_access_token"]
+        if token:
+            return JsonResponse({"token": token})
+    response = JsonResponse({"token": None})
+    return response
+
+
+@auth.jwt_login_required
+def get_some_data(request):
+    token_data = request.payload
+    response = JsonResponse({"token": token_data["user"]})
+    return response
+
+
+@auth.jwt_login_required
+def check_user(request):
+    if request.user is not None:
+        return JsonResponse({"authenticated": request.user.is_authenticated})
+    else:
+        return JsonResponse({"message": "not found"})
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_accounts(request):
+    if request.method == "GET":
+        attendees = User.objects.all()
+        return JsonResponse(
+            {"accounts": attendees},
+            encoder=AccountListEncoder,
+        )
+    else:
+        try:
+            content = json.loads(request.body)
+            nusername = content["username"]
+            npassword = content["password"]
+            nfirstname = content["first_name"]
+            nlastname = content["last_name"]
+            nemail = content["email"]
+            account = User.objects.create_user(
+                username=nusername,
+                password=npassword,
+                email=nemail,
+                first_name=nfirstname,
+                last_name=nlastname,
+            )
+            account.save()
+            login(request, account)
+            return JsonResponse(
+                account,
+                encoder=AccountDetailEncoder,
+                safe=False,
+            )
+        except Exception as response:
+            response = JsonResponse(
+                {
+                    "message": "Credentials not unique. Try another one."
+                }
+            )
+            response.status_code = 405
+            return response
+
+
+def update_censors(request, pk):
+    if request.method == "PUT":
+        account = User.objects.get(id=pk)
+        content = json.loads(request.body)
+        ncensor = content["censored"]
+
+        User.objects.filter(id=pk).update(censored=ncensor)
+
+        account = User.objects.get(id=pk)
+        return JsonResponse(
+            account,
+            encoder=AccountDetailEncoder,
+            safe=False,
+        )
+    else:
+        return JsonResponse({"message": "failed"})
+
+
+@require_http_methods(["DELETE", "PUT", "GET"])
+def api_show_account(request, pk):
+    if request.method == "GET":
+        account = User.objects.get(id=pk)
+        return JsonResponse(account, encoder=AccountDetailEncoder, safe=False)
+    elif request.method == "DELETE":
+        count, _ = User.objects.filter(id=pk).delete()
+        User.objects.filter(id=pk).delete()
+        return JsonResponse({"deleted": count > 0})
+    else:
+        content = json.loads(request.body)
+        nusername = content["username"]
+        npassword = make_password(content["password"])
+        nfirstname = content["first_name"]
+        nlastname = content["last_name"]
+        nemail = content["email"]
+
+        User.objects.filter(id=pk).update(
+            username=nusername,
+            password=npassword,
+            email=nemail,
+            first_name=nfirstname,
+            last_name=nlastname,
+        )
+
+        account = User.objects.get(id=pk)
+        return JsonResponse(
+            account,
+            encoder=AccountDetailEncoder,
+            safe=False,
+        )
+
+
+@require_http_methods(["POST"])
+def neo_authenticate(request):
+    nusername = request.POST["username"]
+    npassword = request.POST["password"]
+    user = authenticate(request, username=nusername, password=npassword)
+    if user is not None:
+        login(request, user)
+        return JsonResponse({"message": "got it"})
+    else:
+        response = JsonResponse(
+            {
+                "message": "Credentials not unique. Try another one."
+            }
+        )
+        response.status_code = 400
+        return response
+
+
+@require_http_methods(["DELETE"])
+def neo_logout(request):
+    logout(request)
+    return JsonResponse({"message": "got it"})
