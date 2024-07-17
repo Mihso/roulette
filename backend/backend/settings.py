4049d99a8aeb4c9de5cda6e053a827a9009e0c59
Matthew Oshimo <moshimo90696@gmail.com>
2024-07-16 19:28:16 -0700

    remove os frontend reference

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,12 +30,10 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
-    os.environ.get("FRONTEND_URL"),
     "http://roulettemihso.s3-website-us-east-1.amazonaws.com"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
-    os.environ.get("FRONTEND_URL"),
     "http://roulettemihso.s3-website-us-east-1.amazonaws.com"
 ]
 
